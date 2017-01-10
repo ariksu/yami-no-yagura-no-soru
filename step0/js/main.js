6f93211569fef7461e3756b5f62ea00402b56f50
Aleksey Ariksu Petrov <ariksu@gmail.com>
2017-01-11 00:56:04 +0300

    refactored a little

--- step0/js/main.js
+++ step0/js/main.js
@@ -1,15 +1,23 @@
 var canvas=document.getElementById('hworld');
 var context=canvas.getContext('2d');
+context.lineWidth = 5;
+context.lineCap = 'round';
 var color='blue';
-var arcstart=0;
 var Pi=Math.PI;
-var rotate_angle = Pi/15;
-var direction=1;
-var actor_x=50;
-var actor_y=50;
-var Game={};
-var mouse_x = 0;
-var mouse_y = 0;
+var Game={
+    arcstart:0,
+    direction:1,
+    actor_x:50,
+    actor_y:50,
+    mouse_x:0,
+    mouse_y:0,
+    draw_color:'yellow',
+    started:false,
+    fps:30
+};
+
+
+
 var KeyFlags={
     left:false,
     right:false,
@@ -21,9 +29,11 @@ var KeyFlags={
 
 canvas.addEventListener('mousemove', function(evt) {
     var mousePos = getMousePos(canvas, evt);
-    mouse_x=mousePos.x;
-    mouse_y=mousePos.y;
-},false);
+    Game.mouse_x=mousePos.x;
+    Game.mouse_y=mousePos.y;
+});
+
+//TODO replace to array
 window.addEventListener('keydown', function (evt) {
     switch (evt.keyCode){
         case 37: KeyFlags.left=true; break;
@@ -41,23 +51,20 @@ window.addEventListener('keyup', function (evt) {
         case 40: KeyFlags.down=false; break;
     }
 });
+
+//TODO create actor class
 Game.update= function() {
-    // if (Math.abs(arcstart)<2*Pi){
-    //     arcstart-=2*Pi*direction;
-    // }
-    arcstart=Math.atan2(mouse_y-actor_y,mouse_x-actor_x)+(Pi/2);
+    Game.arcstart=Math.atan2(Game.mouse_y - Game.actor_y, Game.mouse_x - Game.actor_x)+(Pi/2);
     move();
 };
-Game.fps = 30;
+
 Game.draw= function () {
     context.beginPath();
     context.fillStyle = color;
     context.fillRect(0,0,1200,1300);
-
-    context.strokeStyle = 'yellow';
-    context.lineWidth = 5;
-    context.lineCap = 'round';
-    context.arc(actor_x,actor_y,20,arcstart,arcstart+Pi);
+    context.strokeStyle = Game.draw_color;
+    context.arc(Game.actor_x,Game.actor_y,20,Game.arcstart,Game.arcstart+Pi);
+    console.log(Game.actor_x);
     context.stroke();
 };
 
@@ -77,31 +84,18 @@ function getMousePos(canvas, evt) {
 }
 
 function move(){
-    if (KeyFlags.right==true) { actor_x+=1;}
-    if (KeyFlags.left==true) { actor_x-=1;}
-    if (KeyFlags.up==true) { actor_y-=1;}
-    if (KeyFlags.down==true) { actor_y+=1;}
+    if (KeyFlags.right==true) { Game.actor_x+=1;}
+    if (KeyFlags.left==true) { Game.actor_x-=1;}
+    if (KeyFlags.up==true) { Game.actor_y-=1;}
+    if (KeyFlags.down==true) { Game.actor_y+=1;}
 }
 
-function colorChange(){
-    if (color=='blue') {
-        color='red';
-    }
-    else {
-        color='blue';
-    }
-}
 
-function rotationChange(){
-    direction=-direction;
-}
 
-Game.started=false
 function GameStart() {
     if (Game.started==false) {
         Game._intervalId = setInterval(Game.run, 1000 / Game.fps);
         Game.started=true;
-        keyReset();
     }
 }
 
